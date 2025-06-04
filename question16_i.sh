9ad4a423a12592ba6fb19ab2c58752fe92fff789
Bristy <155566495+briiisty@users.noreply.github.com>
2025-06-04 11:24:26 +0000

    yes

---
+++ question16_i.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p dir16_1/dir16_2/dir16_3/dir16_4
+touch dir16_1/dir16_2/dir16_3/dir16_4/file16_3
+
+touch dir16_1/dir16_2/file16_2
+
+mkdir -p dir16_1/dir16_5
+touch dir16_1/dir16_5/file16_4 dir16_1/dir16_5/file16_5
+
+touch dir16_1/file16_1
---
+++ question16_ii.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -r dir16_1 dir16_1_copy
+
+mv dir16_1/dir16_2/file16_2 dir16_1/dir16_2/file16_2_renamed
+
+mv dir16_1/dir16_2/dir16_3/dir16_4 dir16_1/dir16_2/
+
+rm -r dir16_1/dir16_5
